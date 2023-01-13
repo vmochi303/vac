1982d7a53c5ed791d2bcf1272da62bc560c1d44f
vmochi303 <vmochi303@rku.ac.in>
2023-01-13 22:01:09 +0530

    Create-React-App

---
+++ vac/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">vac</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;
